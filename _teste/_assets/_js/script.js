7d0fd39fcfde485df3b31e296b2b63a4305d8e42
Rafael Dantas <rafael.dantas.br@gmail.com>
2018-05-13 18:13:10 -0300

    First files for contact

--- _teste/_assets/_js/script.js
+++ _teste/_assets/_js/script.js
@@ -1,13 +1,10 @@
 var sideMenu = document.getElementsByClassName('menu__side-menu')[0];
 var sideMenuCloseButton = document.getElementsByClassName('menu__close-button')[0];
-console.log(sideMenuCloseButton);
 
 function checaMenu() {
   if (sideMenu.style.display == 'flex') {
 		sideMenu.style.display = 'none';
-		sideMenuCloseButton.style.display = 'none';
   } else {
 		sideMenu.style.display = 'flex'; 
-		sideMenuCloseButton.style.display = "inline-block	";
   }
 }
