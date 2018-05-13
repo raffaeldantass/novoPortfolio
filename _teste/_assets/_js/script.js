180bfe50309b04a290130ad48abfdb78a3b1bbde
Rafael Dantas <rafael.dantas.br@gmail.com>
2018-05-13 16:13:53 -0300

    Improving the menu behaviour

--- _teste/_assets/_js/script.js
+++ _teste/_assets/_js/script.js
@@ -1,9 +1,13 @@
 var sideMenu = document.getElementsByClassName('menu__side-menu')[0];
+var sideMenuCloseButton = document.getElementsByClassName('menu__close-button')[0];
+console.log(sideMenuCloseButton);
 
 function checaMenu() {
   if (sideMenu.style.display == 'flex') {
-    sideMenu.style.display = 'none';
+		sideMenu.style.display = 'none';
+		sideMenuCloseButton.style.display = 'none';
   } else {
-    sideMenu.style.display = 'flex';
+		sideMenu.style.display = 'flex'; 
+		sideMenuCloseButton.style.display = "inline-block	";
   }
 }
